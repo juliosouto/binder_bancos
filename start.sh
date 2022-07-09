9dadd090342beda86a6c226e348faa48266d76f2
Julio Souto <julio.souto@me.com>
2022-07-09 16:38:59 -0300

    mariadb fix

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "iniciando o servidor jupyter, abra o navegador em http://localhost:8888 e 
 
 repo2docker -p 8888:8888 \
             -v ./docker_vols/:$(echo ~)/local-home \
-            https://gitlab.com/caioau/binder_bancos.git \
+            https://github.com/juliosouto/binder_bancos.git \
             jupyter lab --ip 0.0.0.0 --NotebookApp.token='supersenha'
 
